7fe9b9fea6677231d25fcda59bebf3d27aa415fb
Deric Pan <dericpan97@gmail.com>
2021-05-10 19:56:51 -0400

    Uploading crypto price viewer projet

---
+++ CryptoPriceViewer/CryptoPriceViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceViewerApp.swift
+//  CryptoPriceViewer
+//
+//  Created by Deric Pan on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoinList()
+        }
+    }
+}
